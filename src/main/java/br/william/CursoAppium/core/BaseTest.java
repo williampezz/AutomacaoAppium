d3b323b1cc416c7c8670a8a30cab4788a71416e1
williampez <williamaugustopez@gmail.com>
2023-04-17 18:41:57 -0300

    Add Screenshots for tests

--- src/main/java/br/william/CursoAppium/core/BaseTest.java
+++ src/main/java/br/william/CursoAppium/core/BaseTest.java
@@ -3,6 +3,8 @@ package br.william.CursoAppium.core;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -10,6 +12,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class BaseTest {
+
+    @Rule
+    public TestName testname = new TestName();
 
     @AfterClass
     public static void finalizarClasse() {
@@ -25,7 +30,7 @@ public class BaseTest {
 
         try {
             File imagem =  ((TakesScreenshot)DriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(imagem, new File("target/screenshots/imagem.png"));
+            FileUtils.copyFile(imagem, new File("target/screenshots/"+testname.getMethodName()+".png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
